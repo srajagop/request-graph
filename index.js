00016383db10d69546809b63d1f8355ca41ee024
SanthanaRajagopalan <sanoonline@gmail.com>
2018-05-28 22:14:20 -0700

    removing comments

--- index.js
+++ index.js
@@ -6,32 +6,10 @@ const getDomain = require('./utils');
   const page = await browser.newPage();
   let requests = [];
   let requestMap = {};
-  /*await page.setRequestInterception(true);
-  page.on('request', request => {
-    //allowing through all requests, should (?) be seen as a no-op.
-    //However, coolblue.nl get's scrambled completely.
-	console.log(request.initiator)
-    request.continue();
-  });*/
+
   const client = await page.target().createCDPSession();
   await client.send('Network.enable');
 
-  // added configuration
-  /*await client.send('Network.setRequestInterception', {
-    patterns: [{ urlPattern: '*' }],
-  });
-
-  await client.on('Network.requestIntercepted', async e => {
-    console.log('EVENT INFO: ',e);
-    console.log(e.interceptionId);
-    console.log(e.resourceType);
-    console.log(e.isNavigationRequest);
-
-    // pass all network requests (not part of a question)
-    await client.send('Network.continueInterceptedRequest', {
-      interceptionId: e.interceptionId,
-    });
-  });*/
   await client.on('Network.requestWillBeSent', async e => {
      requests[e.request.url] = e.initiator;
     // console.log(getDomain(e.request.url), " :: ",e.request.url);
