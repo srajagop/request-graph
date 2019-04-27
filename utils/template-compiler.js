7e8556045fd768c8b5fc46f32b062e484c87ec7b
srajagop <santhana_rajagopalan@hotmail.com>
2019-04-27 13:03:54 -0700

    wrong file

--- utils/template-compiler.js
+++ utils/template-compiler.js
@@ -1,12 +1,5 @@
 
 function compile(html) {
-  return function(data) {
-    var out = html
-    data = data || {}
-    Object.keys(data).forEach(function(key) {
-      out = out.replace('<!-- @@' + key + ' -->', data[key])
-    })
-    return out
-  }
+ 
 }
 module.exports = compile
